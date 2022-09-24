9274d816ef77fbfeb78693ca3ab279e916499043
knh4437 <knh4437@naver.com>
2022-09-24 16:40:38 +0900

    Hide API KEY

--- src/main/java/com/CapStone/Backend/Controller/TestController.java
+++ src/main/java/com/CapStone/Backend/Controller/TestController.java
@@ -1,6 +1,7 @@
 package com.CapStone.Backend.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class TestController {
 
+    @Value("${GOOGLE-MAP-KEY}")
+    private String Google_Map_Api;
+
     @GetMapping("/test")
     public Boolean testCallback() {
+        System.out.println("google key 값 : " + Google_Map_Api);
         return true;
     }
+
 }
