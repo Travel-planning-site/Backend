660f19ad5d379326004ab22480cbef8192b09ed2
knh4437 <knh4437@naver.com>
2022-10-03 22:44:45 +0900

    FEAT:Add DaumSearchAPI and request result to Frontend

--- src/main/java/com/CapStone/Backend/Controller/ResearchPlaceController.java
+++ src/main/java/com/CapStone/Backend/Controller/ResearchPlaceController.java
@@ -1,12 +1,17 @@
 package com.CapStone.Backend.Controller;
 
+import com.CapStone.Backend.Dto.PlaceInfoDTO;
 import com.CapStone.Backend.Service.ResearchPlace.ResearchPlaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,11 +21,26 @@ public class ResearchPlaceController {
 
     private final ResearchPlaceService ResearchPlaceService;
 
-    @GetMapping("/search")
-    public void searchPlace(@RequestParam("input") String input) {
+
+    @GetMapping("/searchplace")
+    public ResponseEntity<Boolean> searchPlace(@RequestParam("input") String input) {
         System.out.println("검색어 : " + input);
         ResearchPlaceService.setKeyword(input);
-        ResearchPlaceService.kakaoSearch();
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
+    @GetMapping("/getsearch")
+    public ResponseEntity<ArrayList<ArrayList<PlaceInfoDTO>>> getSearchResult(){
+        return new ResponseEntity<>(searchPhoto(), HttpStatus.OK);
+    }
+
+    // 본래 input은 없어야함. input으로 주소들이 들어오기 때문에
+    public ArrayList<ArrayList<PlaceInfoDTO>> searchPhoto() {
+        return ResearchPlaceService.daumSearch(getplaceList());
+    }
+
+    public ArrayList<ArrayList<PlaceInfoDTO>> getplaceList() {
+        return ResearchPlaceService.kakaoSearch();
     }
 
 }
--- src/main/java/com/CapStone/Backend/Service/ResearchPlace/ResearchPlaceService.java
+++ src/main/java/com/CapStone/Backend/Service/ResearchPlace/ResearchPlaceService.java
@@ -1,28 +1,47 @@
 package com.CapStone.Backend.Service.ResearchPlace;
 
+import com.CapStone.Backend.Dto.PlaceInfoDTO;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.ArrayList;
 
 @RequiredArgsConstructor
 @Service
+@Setter
 public class ResearchPlaceService {
 
-    private final KakaoSearchService KakaoSearchService;
+    private final KakaoSearchService kakaoSearchService;
+    private final KakaoPhotoService kakaoPhotoService;
     private String keyword;
 
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
+    public ArrayList<ArrayList<PlaceInfoDTO>> kakaoSearch() {
+        // 카카오 로컬 API를 이용한 장소 검색
+        int pageAmount = 3;
+        ArrayList<ArrayList<PlaceInfoDTO>> placeList = new ArrayList<>();
+        Gson gson = new Gson();
+
+//        System.out.println("ResearchPlaceService_kakaoSearch keyword 값 : " + keyword);
+        for (int i = 1; i <= pageAmount; i++) {
+            placeList.add(kakaoSearchService.getKakaoSearch(keyword, i));
+        }
+//        System.out.println("[ResearchPlaceService]검색된 총 장소 갯수 : " + placeList.size());
+
+        return placeList;
     }
 
-    public void kakaoSearch() {
-        int pageAmount = 3;
-        System.out.println("ResearchPlaceService_kakaoSearch keyword 값 : " + keyword);
-        for (int i = 1; i <= pageAmount; i++) {
-            KakaoSearchService.getKakaoSearchObject(keyword, i);
+
+    public ArrayList<ArrayList<PlaceInfoDTO>> daumSearch(ArrayList<ArrayList<PlaceInfoDTO>> placeLists) {
+        // 다음 검색 API를 이용한 장소 사진
+        ArrayList<PlaceInfoDTO> placeList;
+        for (int i=0;i<placeLists.size();i++) {
+            placeList = placeLists.get(i);
+            placeLists.set(i, kakaoPhotoService.requestKakaoPhoto(placeList));
+            // placeLists 수정
         }
+        return placeLists;
     }
 
 }
