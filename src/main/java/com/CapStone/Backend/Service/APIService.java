054abfa8feb3786a020c64e29b1bab0c82761263
knh4437 <knh4437@naver.com>
2022-11-17 09:12:02 +0900

    fix: add Value Annotation

--- src/main/java/com/CapStone/Backend/Service/APIService.java
+++ src/main/java/com/CapStone/Backend/Service/APIService.java
@@ -6,6 +6,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
